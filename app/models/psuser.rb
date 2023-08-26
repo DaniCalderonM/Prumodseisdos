57d2b88c348047ac5069d4b5f5337cc9795ab3e3
danicalderonm <mesiasd.calderon@gmail.com>
2023-08-25 20:52:37 -0400

    ajustando

--- app/models/psuser.rb
+++ app/models/psuser.rb
@@ -4,7 +4,7 @@ class Psuser < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :applies
+         has_many :applies, dependent: :destroy
          has_many :psposts, through: :applies, dependent: :destroy
          has_one_attached :avatar
          has_many :notifications, as: :recipient, dependent: :destroy
